0a690d16702c7f7e05bad5e41ee4d6451c9d35b6
raja-spec <rraja48569@gmail.com>
2023-08-17 21:00:36 +0530

    first commit

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
