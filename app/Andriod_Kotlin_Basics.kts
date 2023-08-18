90d9fa692f22a475f322f632a87ec9564e305325
raja-spec <rraja48569@gmail.com>
2023-08-18 20:38:48 +0530

    About Kotlin notes

---
+++ app/Andriod_Kotlin_Basics.kts
@@ -0,0 +1,23 @@
+//           ABOUT KOTLIN
+/*
+
+  * Kotlin is statically typed language that runs
+    on JVM
+
+  * Java can be integrated with kotlin and functions
+    of java can be overridden by kotlin.
+
+  * Lenght of the code will be reduced in kotlin
+    compare to java
+
+
+           FEATURES OF KOTLIN
+
+   * Concise (less code)
+   * Interoperable
+   * Trustworthy
+   * Easy
+   * Less error
+
+
+ */
